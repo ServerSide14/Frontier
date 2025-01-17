2f5e908a12ccf5bde99ea1ed5a102f6b373ca7d1
ServerSide14 <loreserverSS14@gmail.com>
2025-01-17 21:07:11 +0300

    serverside

--- runserver - Release.bat
+++ runserver - Release.bat
@@ -1,3 +1,3 @@
 @echo off
-dotnet run --project Content.Server --configuration Release
+dotnet run --project Content.Server --configuration Release --config-file .\ServerFiles\ConfigFrontier.toml --data-dir .\ServerFiles\Database
 pause
